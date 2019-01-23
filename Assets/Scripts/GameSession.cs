de5c04e7881109eafcc526a0557a15e05884f013
simonhomoludens <liusihui@outlook.com>
2019-01-23 18:43:40 +0800

    5th Version_UIHalfCompleted

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameSession : MonoBehaviour {
+public class GameSession : MonoBehaviour
+{
 
     [SerializeField]
     private int playerLives = 3;
+    [SerializeField]
+    private Text scoreText, lifeText;
+
+    private int score = 0;
 
     private void Awake()
     {
         int numGameSession = FindObjectsOfType<GameSession>().Length;
-        if (numGameSession>1)
+        if (numGameSession > 1)
         {
             Destroy(gameObject);
         }
@@ -23,8 +29,17 @@ public class GameSession : MonoBehaviour {
     }
 
 
-    public void ProcessPlayerDeath() {
-        if (playerLives>1)
+    private void Start()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        lifeText.text = "Life X " + playerLives.ToString();
+
+    }
+
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
         {
             TakeLife();
         }
@@ -45,7 +60,15 @@ public class GameSession : MonoBehaviour {
     private void TakeLife()
     {
         playerLives--;
+        lifeText.text = "Life X " + playerLives.ToString();
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = "Score: " + score.ToString();
     }
 }
